4567e1014d1238f1572565cb41ce089547324bd7
double joker <double@163.com>
2022-01-08 23:26:20 +0800

    jprotobuf

--- case/README.md
+++
@@ -1,5 +0,0 @@
-![img](https://img.shields.io/badge/JDK-17-green.svg)
-
-实践案例
-
-tank
---
+++ game-collect/game-common-proto/src/main/java/com/iohao/game/collect/proto/Teacher.java
@@ -0,0 +1,60 @@
+package com.iohao.game.collect.proto;
+
+import com.baidu.bjf.remoting.protobuf.Codec;
+import com.baidu.bjf.remoting.protobuf.ProtobufIDLGenerator;
+import com.baidu.bjf.remoting.protobuf.ProtobufProxy;
+import com.baidu.bjf.remoting.protobuf.annotation.EnableZigZap;
+import com.baidu.bjf.remoting.protobuf.annotation.Protobuf;
+import com.baidu.bjf.remoting.protobuf.annotation.ProtobufClass;
+import lombok.Data;
+
+import java.io.IOException;
+
+/**
+ * ssssl
+ *
+ * @author 洛朱
+ * @date 2022-01-07
+ */
+@Data
+@ProtobufClass(description = "老师")
+@EnableZigZap
+public class Teacher {
+    /** name */
+    @Protobuf(description = "姓名")
+    String name;
+    int id;
+    long age;
+    @Protobuf(description = "邮箱")
+    String email;
+
+    Double doubleF;
+    Float floatF;
+    byte[] bytesF;
+
+    Boolean boolF;
+
+    public static void main(String[] args) {
+        Codec<Teacher> teacherCodec = ProtobufProxy.create(Teacher.class);
+
+        Teacher teacher = new Teacher();
+        teacher.id = 1;
+        teacher.name = "alex";
+
+        try {
+            byte[] encode = teacherCodec.encode(teacher);
+
+            Teacher teacherParse = teacherCodec.decode(encode);
+
+            System.out.println(teacherParse);
+
+            String code = ProtobufIDLGenerator.getIDL(Teacher.class);
+
+            System.out.println(code);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
